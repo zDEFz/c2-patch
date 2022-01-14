e8fbe4d7cc244292c18f9170da42b71fe55d205d
zDEFz <defenz0r@web.de>
2022-01-14 15:06:48 +0100

    Delete Win-Update-Cultris2.bat

--- launchers/Win-Update-Cultris2.bat
+++
@@ -1,4 +0,0 @@
-ECHO starting update in 10 seconds. Make sure you close Cultris II before updating.
-CHOICE /T 10 /C YN /D Y
-".\resources\jdk-19-win\bin\java.exe" -cp cultris2.jar updater
-TIMEOUT /T 10
