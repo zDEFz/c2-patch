e2f70bec345f665c92ee7ba5dbf86f98be817a5d
bluetail <a-development+archpkg@posteo.de>
2023-08-23 17:29:38 +0200

    initial 5

--- launchers/Linux-32-Update-Cultris2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo starting update in 10 seconds. Make sure you close Cultris II before updating.
-"./resources/jdk-19-lin/bin/java" -Djava.library.path=./libs -cp cultris2.jar updater
