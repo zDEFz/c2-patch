bb44d7014b72f606322cbd39911a8967034b3289
bluetail <a-development+archpkg@posteo.de>
2023-08-23 17:23:23 +0200

    initial 3

--- Linux-32-Start cultris2 Tool.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-"./resources/adoptium-jdk17-x86/bin/java" -cp cultris2.jar c2settings
--- launchers/Linux-32-Start cultris2.sh
+++ launchers/Linux-32-Start cultris2.sh
@@ -4,7 +4,7 @@ export DIR="$(dirname "$(readlink -f "$0")")"  # Get the script's directory
 cd "$DIR"
 
 if [ -f "$DIR/resources/adoptium-jdk17-x86/bin/java" ]; then
-    "$DIR/resources/adoptium-jdk17-x86/bin/java" -Djava.library.path="$DIR/resources/libs/" -jar "$DIR/cultris2.jar"
+    "$DIR/resources/adoptium-jdk17-x86/bin/java" -Djava.library.path="$DIR/resources/libs/" -jar "$DIR/c2-patch-clean.jar"
 else
     echo "Java executable not found."
 fi
