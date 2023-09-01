96b72da591aa41fa5bc5d97a7e40b57ae07d78b5
bluetail <a-development+archpkg@posteo.de>
2023-09-02 00:08:56 +0200

    renamed win jdk installer

---
+++ setup/w10-to-w11-jdk17-installer.bat
@@ -0,0 +1,11 @@
+@echo off
+cd \..\resources\
+
+:: Download the JDK
+wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8%2B7/OpenJDK17U-jdk_x64_windows_hotspot_17.0.8_7.zip
+
+:: Unzip the downloaded file
+powershell -command "Expand-Archive -Path OpenJDK17U-jdk_x64_windows_hotspot_17.0.8_7.zip -DestinationPath ."
+
+:: Remove the downloaded ZIP file
+del OpenJDK17U-jdk_x64_windows_hotspot_17.0.8_7.zip
