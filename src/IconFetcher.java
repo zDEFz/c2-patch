5022e24233fd43bb0ab91fb5e7e3d3447dc2660e
zDEFz <a-development@posteo.de>
2024-02-16 11:24:51 +0100

    gravatar:default picture redirected to the T piece image

--- src/IconFetcher.java
+++ src/IconFetcher.java
@@ -1,11 +1,8 @@
 public class IconFetcher {
 
     public static String fetchIcon(String md5Hash) throws Exception {
-        // Debug statement to print the input MD5 hash
-        System.out.println("Input MD5 hash: " + md5Hash);
-
         // Construct the URL using the MD5 hash
-        String url = "https://www.gravatar.com/avatar/" + md5Hash + ".jpg";
+        String url = "https://www.gravatar.com/avatar/" + md5Hash + "?d=https://i.imgur.com/Gms07El.png";
 
         // Debug statement to print the constructed URL
         System.out.println("Constructed URL: " + url);
