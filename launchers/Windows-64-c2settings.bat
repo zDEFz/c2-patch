895008bef9e848dc8fd85b029cd32e73df162c7b
bluetail <a-development+gitea@posteo.de>
2024-11-23 00:09:41 +0100

    Launchers: Windows - Auto locate java.exe

--- launchers/Windows-64-c2settings.bat
+++ launchers/Windows-64-c2settings.bat
@@ -1,2 +1,26 @@
 @echo off
-.\resources\jdk-17.0.9+9\bin\java.exe -cp "cultris2.jar" c2settings
+setlocal enabledelayedexpansion
+
+rem Set the base directory to search for Java executables
+set "baseDir=.\resources"
+set "javaExe=bin\java.exe"
+set "latestDir="
+
+rem Loop through all subdirectories in the base directory
+for /D %%I in ("%baseDir%\*") do (
+    rem Check if the directory contains the Java executable
+    if exist "%%I\%javaExe%" (
+        set "latestDir=%%I"
+    )
+)
+
+rem If a valid Java directory is found, execute the ColorPicker
+if defined latestDir (
+    echo Found Java in: !latestDir!
+    start "c2settings" /high "!latestDir!\%javaExe%" -cp "cultris2.jar" c2settings
+) else (
+    echo No directory with java.exe found in the resources folder.
+    pause
+)
+
+endlocal
